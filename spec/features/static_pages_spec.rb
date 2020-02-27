96ce91d94b745b35cf5c229959ea37e5bd9675ae
terufumi1122 <terufumi1122@gmail.com>
2020-02-28 08:09:59 +0900

    chore: add static_pages/contact

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,23 +3,30 @@ require 'rails_helper'
 RSpec.feature "StaticPages", type: :feature do
 
   describe 'Home' do
-    specify '画面の表示' do
+    specify 'タイトル内容の表示' do
       visit '/static_pages/home'
-      expect(page).to have_css('h1', text: 'Sample App')
+      expect(page).to have_title 'Home | Ruby on Rails Tutorial Sample App'
     end
   end
 
   describe 'Help' do
-    specify '画面の表示' do
+    specify 'タイトル内容の表示' do
       visit '/static_pages/help'
-      expect(page).to have_css('h1', text: 'Help')
+      expect(page).to have_title 'Help | Ruby on Rails Tutorial Sample App'
     end
   end
 
   describe 'About' do
-    specify '画面の表示' do
+    specify 'タイトル内容の表示' do
       visit '/static_pages/about'
-      expect(page).to have_css('h1', text: 'About')
+      expect(page).to have_title 'About | Ruby on Rails Tutorial Sample App'
+    end
+  end
+
+  describe 'Contact' do
+    specify 'タイトル内容の表示' do
+      visit '/static_pages/contact'
+      expect(page).to have_title 'Contact | Ruby on Rails Tutorial Sample App'
     end
   end
 
